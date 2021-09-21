bdd8cb63a0f2d42717bc492da311f8c9300d2282
Ryuzaki13 <ryuzaki_dk_rm_guitar@mail.ru>
2021-09-21 15:07:01 +0500

    Правки перемещения

--- script.js
+++ script.js
@@ -8,14 +8,25 @@ if (buttonCreateWindow) {
     // Для этой кнопки установить событие на клик мыши
     buttonCreateWindow.onclick = createWindow;
 }
-// Переменная-счетчик необходима для установки позиции по осbb
+// Переменная-счетчик необходима для установки позиции по оси Z активного окна
+// чтобы активное окно было всегда повех всех остальных
 let zIndex = 1;
-
-
 /**
+ * Создание нового окна
  * @param {MouseEvent} event
  */
 function createWindow(event) {
+    /**
+     * Создание структуры HTML
+     * <div class="window">
+     *     <div class="head">
+     *         <div>Текст заголовка окна</div>
+     *         <div class="close"></div>
+     *     </div>
+     *     <div class="body"></div>
+     * </div>
+     */
+
     let rootWindow = document.createElement("div"),
         head = document.createElement("div"),
         body = document.createElement("div"),
@@ -24,6 +35,8 @@ function createWindow(event) {
 
     rootWindow.append(head, body);
     head.append(headerText, close);
+
+    // События на заголовок для перемещения окна
     head.onmousedown = onMouseDown;
     head.onmousemove = onMouseMove;
     head.onmouseup = onMouseUp;
@@ -35,38 +48,44 @@ function createWindow(event) {
     body.className = "body";
     close.className = "close";
 
+    // Событие на кнопку "Закрыть"
     close.onclick = closeWindow;
 
+    // Добавить созданное окно в конец тела страницы
     document.body.append(rootWindow);
 }
-
 /**
  * @param {MouseEvent} event
  */
 function closeWindow(event) {
+    // Для закрытия окна получаем родительский элемент текущего
+    // элемента (т.е. кнопки закрыть) с классом .window
     let thisWindow = this.closest(".window");
     if (thisWindow) {
+        // И если его нашли - удаляем
         thisWindow.remove();
     }
 }
-
 /**
  * @param {MouseEvent} event
  */
 function onMouseDown(event) {
+    // Установить флажок на текущее окно, говоря, что его можно перемещать
+    // Событие onmousemove будет работать для этого элемента, если значение "true
     this.dataset.isMove = "true";
-    this.dataset.x = event.clientX;
-    this.dataset.y = event.clientY;
+    // Сохранить текущую позицию мыши, чтобы потом найти разницу между предыдущей
+    // и текущей позицией мыши
+    this.dataset.x = "" + event.clientX;
+    this.dataset.y = "" + event.clientY;
+    // Установить на текущее окно позицию по оси Z выше, чем у кого-либо
     this.parentElement.style.zIndex = "" + (zIndex++);
 }
-
 /**
  * @param {MouseEvent} event
  */
 function onMouseUp(event) {
     this.dataset.isMove = "false";
 }
-
 /**
  * @param {MouseEvent} event
  */
